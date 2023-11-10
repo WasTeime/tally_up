c5ddd1b02d1178d100321bc51f9cc6d4d3f04012
WasTeime <miracleup10@gmail.com>
2023-11-11 00:49:57 +0300

    init 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
@@ -19,8 +18,6 @@ class TallyUp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: 
-    );
+    return MaterialApp(theme: GetIt.I<AppTheme>().currentTheme);
   }
 }
