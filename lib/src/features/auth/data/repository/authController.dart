52d822a83223101d112837cc0b14bcd0900cfcc0
WasTeime <miracleup10@gmail.com>
2023-11-19 20:16:58 +0300

    добавил crashlytics

--- lib/src/features/auth/data/repository/authController.dart
+++ lib/src/features/auth/data/repository/authController.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart';
 
 class AuthController {
@@ -26,6 +27,7 @@ class AuthController {
         //   _verificationFailedController.add("Ой что-то пошло не так");
         // }
         _verificationFailedController.add("Ой что-то пошло не так");
+        FirebaseCrashlytics.instance.recordError(e, e.stackTrace);
       },
       codeSent: (String verificationId, int? resendToken) {
         _verificationId = verificationId;
